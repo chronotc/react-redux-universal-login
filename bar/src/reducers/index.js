17d821440dc1a591b59224af29981499f0c039b0
chronotc <7519144+chronotc@users.noreply.github.com>
2018-06-21 21:41:59 +1000

    remove change password and forgot

--- bar/src/reducers/index.js
+++ bar/src/reducers/index.js
@@ -2,17 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import auth from './authReducer';
 import callback from './callbackReducer';
-import forgot from './forgotPasswordReducer';
 import home from './homeReducer';
-import password from './changePasswordReducer';
 
 const rootReducer = combineReducers({
   form,
   auth,
   callback,
-  forgot,
-  home,
-  password
+  home
 });
 
 export default rootReducer;
